686c5f15ee28419ec101627ce026a56b94a1e630
Shereen789 <irfanashereen@msitprogram.net>
2020-04-13 13:23:17 +0530

    adding Test cases

--- lru_cache.py
+++ lru_cache.py
@@ -6,7 +6,12 @@ class LRU:
         #cache is the ordered dictionary we are using to store data
         self.cache = collections.OrderedDict()
     def put(self,key,value):
-        pass
+        if key not in self.cache.keys():
+            if len(self.cache)>=self.cachesize:
+                #Remove and return a (key, value) pair from the dictionary.
+                #Pairs are returned in LIFO order if last is true or FIFO order if false
+                 self.cache.popitem(last = False)
+        self.cache[key] = value
     def get(self,key):
         pass
     def getcache(self):
---
+++ lru_test.py
@@ -0,0 +1,15 @@
+from lru_cache import LRU
+test = LRU(5)
+test.put(4,"Java")
+test.put(1,"CT")
+test.put(2,"CSPP1")
+test.put(3,"IDS")
+assert test.getcache() == {4:"Java",1:"CT",2:"CSPP1",3:"IDS"}
+print("Assert 1 passed")
+test.put(2,"Python")
+assert test.getcache() == {4:"Java",1:"CT",2:"Python",3:"IDS"}
+print("Assert 2 passed")
+test.put(5,"ADS")
+test.put(6,"DataBases")
+assert test.getcache() == {1:"CT",2:"Python",3:"IDS",5:"ADS",6:"DataBases"}
+print("Assert 3 passed")
