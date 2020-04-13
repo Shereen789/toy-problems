6dda92d378a3cbba9690d0c6da57d1e480e79d10
Shereen789 <irfanashereen@msitprogram.net>
2020-04-13 12:46:05 +0530

    adding method signatures

--- lru_cache.py
+++ lru_cache.py
@@ -5,3 +5,9 @@ class LRU:
         self.cachesize = size
         #cache is the ordered dictionary we are using to store data
         self.cache = collections.OrderedDict()
+    def put(self,key,value):
+        pass
+    def get(self,key):
+        pass
+    def getcache(self):
+        pass
